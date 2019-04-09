a5f295cec63f11dc7402b3b5204550bf0361b74a
andrius.kaliacius <andrius.kaliacius@smarsh.com>
2019-04-09 16:35:08 +0100

    update

--- people-service/src/main/java/com/cognia/peopleservice/code/Beans.java
+++ people-service/src/main/java/com/cognia/peopleservice/code/Beans.java
@@ -3,6 +3,7 @@ package com.cognia.peopleservice.code;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class Beans {
@@ -11,6 +12,7 @@ public class Beans {
     private PersonProps personProps;
 
     @Bean
+    @Scope("prototype")
     public Person person(){
         Person p = new Person();
         p.setFirstName(personProps.getFirstName());
--- people-service/src/main/java/com/cognia/peopleservice/code/Controller.java
+++ people-service/src/main/java/com/cognia/peopleservice/code/Controller.java
@@ -2,20 +2,29 @@ package com.cognia.peopleservice.code;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
-@RestController()
+@RestController
 public class Controller {
 
     @Autowired
     private Person person;
 
-    @GetMapping(("/service"))
-    public Person person(){
-        return person;
+    @Value("${person.firstName}")
+    private String name;
+
+    @GetMapping("/service")
+    public @ResponseBody Person person(){
+        return this.person;
+    }
+
+    @GetMapping("/test")
+    public String show(){
+        return this.name;
     }
 }
